158db5675bddd09b3e00ebdcf638cc563ea182fd
Cameron Stewart <cstewart@pivotal.io>
2016-10-25 16:48:02 -0600

    install hugo take 2

--- ci/tasks/build-hugo.sh
+++ ci/tasks/build-hugo.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-brew update && brew install hugo
+git clone https://github.com/spf13/hugo
+mkdir -p src/github.com/spf13
+ln -sf $(pwd) src/github.com/spf13/hugo
 
-cd ~/playground/prp-days-test
+# set the build path for Go
+export GOPATH=$(pwd)
+
+go get
+
+go build -o hugo main.go
+
+cd ../..
 hugo -t pivotal -v -D
