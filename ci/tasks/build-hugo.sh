33128bf986a5f7d5a99311d26adda355811b9a71
Cameron Stewart <cstewart@pivotal.io>
2016-10-27 16:35:23 -0600

    remove proxy

--- ci/tasks/build-hugo.sh
+++ ci/tasks/build-hugo.sh
@@ -3,7 +3,6 @@ set -e
 
 
 echo "hello cami"
-export ALL_PROXY=24.8.194.104:443
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo "downloaded homebrew!"
