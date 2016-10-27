506fe829ce2e07fc5257453e9096d771a81c5d52
Cameron Stewart <cstewart@pivotal.io>
2016-10-27 17:32:32 -0600

    ugh 2

--- ci/tasks/build-hugo.sh
+++ ci/tasks/build-hugo.sh
@@ -4,13 +4,10 @@ set -e
 
 echo "hello cami"
 
-useradd -m linuxbrew
-sudo -u linuxbrew -i /bin/bash
-PATH=~/.linuxbrew/bin:/usr/sbin:/usr/bin:/sbin:/bin
-yes | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 
+brew update
 brew install hugo
 
 # export GOPATH=$(pwd)
