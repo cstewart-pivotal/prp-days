a7aa726a979bb2b1f5c6fd631823fbac4567deae
Cameron Stewart <cstewart@pivotal.io>
2016-10-27 15:38:51 -0600

    test homebrew

--- ci/tasks/build-hugo.sh
+++ ci/tasks/build-hugo.sh
@@ -3,9 +3,15 @@ set -e
 
 # set the build path for Go
 echo "hello cami"
-export GOPATH=$(pwd)
 
-go get -v github.com/spf13/hugo
+ruby <(curl -fsSk https://raw.github.com/mxcl/homebrew/go)
+echo "downloaded homebrew?"
+
+brew install hugo
+
+# export GOPATH=$(pwd)
+#
+# go get -v github.com/spf13/hugo
 
 echo "downloaded hugo?"
 echo hugo version
