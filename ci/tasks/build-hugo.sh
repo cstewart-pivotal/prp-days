4418197a2d601eb510f36df1f9d757089e0f76fb
Cameron Stewart <cstewart@pivotal.io>
2016-10-27 16:28:39 -0600

    add proxy and port

--- ci/tasks/build-hugo.sh
+++ ci/tasks/build-hugo.sh
@@ -3,7 +3,7 @@ set -e
 
 
 echo "hello cami"
-export ALL_PROXY=proxyIP:port
+export ALL_PROXY=24.8.194.104:80
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo "downloaded homebrew!"
